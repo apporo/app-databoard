5f2d94ac43d333f2844d26aac53fadef0b054f48
pnhung177 <pnhung177@drupalex.net>
2016-03-10 10:09:52 +0700

    Make configBuilder that create configuration files for ng-admin & ng-nimda

--- config/profile.js
+++ config/profile.js
@@ -1,2 +1,13 @@
 module.exports = {
+  databoard: {
+    client: {
+      "ng-admin": {
+        homedir: "public/client/ng-admin-demo"
+      },
+      "ng-nimda": {
+        homedir: "public/client/ng-nimda-demo"
+      }
+    },
+    default: "ng-admin"
+  }
 };
