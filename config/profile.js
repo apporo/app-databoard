0c4b1714ddb0b9d91c0602b85a720fb1ea03cd24
Pham Ngoc Hung <pnhung177@drupalex.net>
2016-03-31 17:08:15 +0700

    Integrate ng-admin as webserver middleware

--- config/profile.js
+++ config/profile.js
@@ -1,13 +1,2 @@
 module.exports = {
-  databoard: {
-    client: {
-      "ng-admin": {
-        homedir: "public/client/ng-admin-demo"
-      },
-      "ng-nimda": {
-        homedir: "public/client/ng-nimda-demo"
-      }
-    },
-    default: "ng-admin"
-  }
 };
--- config/sandbox.js
+++ config/sandbox.js
@@ -1,2 +1,16 @@
 module.exports = {
+  plugins: {
+    appDataboard: {
+      contextPath: '/databoard',
+      client: {
+        "ng-admin": {
+          homedir: "public/client/ng-admin-demo"
+        },
+        "ng-nimda": {
+          homedir: "public/client/ng-nimda-demo"
+        }
+      },
+      default: "ng-admin"
+    }
+  }
 };
